6a2d5a251e7b97e707938675fa19b6c565b487e6
moro <ricardoromero4444@gmail.com>
2025-02-01 14:37:22 -0300

    feat: add defaultValue to tokenselect component

--- app/exchange.tsx
+++ app/exchange.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { ActivityIndicator, View } from "react-native";
+import { ActivityIndicator, Text } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import styled from "styled-components/native";
 import { useLocalSearchParams } from "expo-router";
@@ -7,20 +7,21 @@ import { useLocalSearchParams } from "expo-router";
 import { Gutter } from "@/constants/Layout";
 import { Colors } from "@/constants/Colors";
 import { useTokenMarkets } from "@/hooks/useTokenMarkets";
+import { formatCurrency } from "@/utils";
 
 export default function ExchangeScreen() {
   const { tokenId } = useLocalSearchParams<{ tokenId?: string }>();
 
   return (
     <Container>
-      <TokenSelect />
-      <TokenSelect />
+      <TokenSelect defaultValue="bitcoin" />
+      <TokenSelect defaultValue="tether" />
     </Container>
   );
 }
 
-const TokenSelect = () => {
-  const [selectedToken, setSelectedToken] = useState<string>("bitcoin");
+const TokenSelect = ({ defaultValue }: { defaultValue: string }) => {
+  const [selectedToken, setSelectedToken] = useState<string>(defaultValue);
   const [amount, setAmount] = useState<string>("0");
   const { data, isLoading } = useTokenMarkets();
 
@@ -35,33 +36,54 @@ const TokenSelect = () => {
     );
 
   return (
-    <SelectContainer>
-      <TokenIcon source={{ uri: currentToken.image }} />
+    <SelectWrapper>
+      <GreyBox>
+        <TokenIcon source={{ uri: currentToken.image }} />
 
-      <InputsWrapper>
-        <StyledPicker
-          selectedValue={selectedToken}
-          onValueChange={(itemValue: string) => setSelectedToken(itemValue)}
-        >
-          {data.map((token) => (
-            <Picker.Item
-              key={token.id}
-              label={token.symbol.toUpperCase()}
-              value={token.id}
-            />
-          ))}
-        </StyledPicker>
+        <InputsWrapper>
+          <StyledPicker
+            selectedValue={selectedToken}
+            onValueChange={(itemValue: string) => setSelectedToken(itemValue)}
+          >
+            {data.map((token) => (
+              <Picker.Item
+                key={token.id}
+                label={token.symbol.toUpperCase()}
+                value={token.id}
+              />
+            ))}
+          </StyledPicker>
 
-        <AmountInput
-          keyboardType="numeric"
-          value={amount}
-          onChangeText={(text: string) => setAmount(text)}
-          placeholder="Amount"
-        />
-      </InputsWrapper>
-    </SelectContainer>
+          <AmountInput
+            keyboardType="numeric"
+            value={amount}
+            onChangeText={(text: string) => setAmount(text)}
+            placeholder="Amount"
+          />
+        </InputsWrapper>
+      </GreyBox>
+
+      <PriceText>
+        1 {currentToken.symbol.toUpperCase()} ={" "}
+        <Text style={{ color: Colors.darkGreen }}>
+          {formatCurrency(currentToken.current_price)}
+        </Text>
+      </PriceText>
+    </SelectWrapper>
   );
 };
+
+const PriceText = styled.Text`
+  color: rgb(184, 184, 184);
+`;
+
+const SelectWrapper = styled.View`
+  flex: 1;
+  flex-direction: columnkj;
+  height: fit-content;
+  place-items: center;
+  gap: 4px;
+`;
 
 const Container = styled.View`
   flex: 1;
@@ -72,15 +94,13 @@ const Container = styled.View`
   background-color: ${Colors.white};
 `;
 
-const SelectContainer = styled.View`
-  flex: 1;
+const GreyBox = styled.View`
   flex-direction: row;
   align-items: center;
   gap: 12px;
   background-color: ${Colors.lightGrey};
   border-radius: 12px;
   padding: 13px 16px;
-  height: fit-content;
 `;
 
 // A wrapper for both the Picker and the TextInput
@@ -92,11 +112,13 @@ const InputsWrapper = styled.View`
 // Restrict or style the Picker's size to prevent overflow
 const StyledPicker = styled(Picker)`
   width: 100%;
-  background-color: ${Colors.lightGrey};
+  border-radius: 4px;
+  background-color: transparent;
   border: none;
   font-weight: bold;
   font-size: 14px;
   line-height: 24px;
+  padding: 0 4px;
 `;
 
 // Basic numeric input styling
@@ -104,7 +126,8 @@ const AmountInput = styled.TextInput`
   width: 100%;
   padding: 0 4px;
   border-radius: 4px;
-  color: ${Colors.darkBlue};
+  color: ${Colors.darkGrey};
+  padding: 4px;
 `;
 
 const TokenIcon = styled.Image`
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,7 +3,9 @@ export { Colors };
 const Colors = {
   white: "#fff",
   lightGrey: "#F1F3FA",
+  darkGrey: "#7A7291",
   darkBlue: "#12033A",
   red: "#DC143C",
   green: "#23EBC3",
+  darkGreen: "#10ad8e",
 } as const;
