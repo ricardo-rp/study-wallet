f83828356d2250799a3feca57f6159ce26f73ee8
moro <ricardoromero4444@gmail.com>
2025-02-01 11:33:07 -0300

    feat: add floating exchange button

--- components/FloatingButton.tsx
+++ components/FloatingButton.tsx
@@ -1,11 +1,32 @@
-import { useRouter } from "expo-router";
+import { Colors } from "@/constants/Colors";
+import { Link } from "expo-router";
+import { ArrowsRightLeftIcon } from "react-native-heroicons/outline";
+import styled from "styled-components/native";
+
+export { FloatingButton };
 
 const visible = true;
 
-export const FloatingButton = () => {
-  const router = useRouter();
-
+const FloatingButton = () => {
   if (!visible) return null;
 
-  return <>hiii</>;
+  return (
+    <Link href="/exchange" asChild>
+      <Pressable>
+        <ArrowsRightLeftIcon stroke="white" width={31} />
+      </Pressable>
+    </Link>
+  );
 };
+
+const Pressable = styled.Pressable`
+  background: ${Colors.green};
+  border-radius: 16px;
+  width: 58px;
+  height: 58px;
+  place-items: center;
+  display: grid;
+  position: absolute;
+  bottom: 52px;
+  right: 28px;
+`;
